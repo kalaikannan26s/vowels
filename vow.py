54e91752fdcfd59a0a81ae689321965c41e38bf4
kalaikannan26s <51432038+kalaikannan26s@users.noreply.github.com>
2019-06-06 12:11:17 +0530

    Create vow.py

---
+++ vow.py
@@ -0,0 +1,7 @@
+ch=str(input("Enter a character:"))
+if(ch=='A'  or ch=='a' or ch=='E' or ch=='e' or ch=='I' or ch=='i' or ch=='O' or ch=='0' or ch=='U' or ch=='u'):
+                print("is a vowel")
+    
+else:
+                print( "invalid consonant")
+
